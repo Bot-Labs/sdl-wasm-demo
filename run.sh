73dfd3495ee8bad02db6fc597c95a24a80a23f90
snandasena <sajithgithub@gmail.com>
2021-09-14 22:08:32 +0530

    Improved

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
-EMSCRIPTEN_CMAKE="/home/sajith/wasm/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake"
+EMSDK_HOME=/home/sajith/wasm/emsdk # Change here
+
+source ${EMSDK_HOME}/emsdk_env.sh
+
+EMSCRIPTEN_CMAKE="${EMSDK_HOME}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake"
 cmake ../ -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN_CMAKE
 
 make
