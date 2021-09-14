6eaa559258d44128486daaab0952e5930e8b77b4
snandasena <sajithgithub@gmail.com>
2021-09-14 22:03:21 +0530

    Improved

---
+++ run.sh
@@ -0,0 +1,6 @@
+EMSCRIPTEN_CMAKE="/home/sajith/wasm/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake"
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN_CMAKE
+
+make
+
+emrun index.html
